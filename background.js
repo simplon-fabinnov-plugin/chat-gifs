d85bd3e998f71e24752fdb4d8281d3cfdb90b596
simplon-davidt <dtarcz@gmail.com>
2015-12-18 11:01:38 +0100

    mise à jour extension.xpi

--- background.js
+++ background.js
@@ -5,6 +5,6 @@
 chrome.browserAction.setBadgeText({text: '(:/)'});
 chrome.browserAction.setBadgeBackgroundColor({color: '#eae'});
 
-chrome.browserAction.onClicked.addlistener(function(aTab) {
+chrome.browserAction.onClicked.addListener(function(aTab) {
     chrome.tabs.create({'url': 'http://chilloutandwatchsomecatgifs.com/', 'active': true});
 });
